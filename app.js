71df9f8f75bc548c980458a03fe0eff752e0a363
Karthik Viswanathan <karthik.ksv@gmail.com>
2016-11-07 13:56:57 -0800

    Switch to ES6.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
+const express = require('express')
 
-var app = express();
+const app = express()
 // serve all files out of public folder
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // TODO: soundcloud finder code
 
-var port = 3000;
-console.log('Listening at 127.0.0.1:' + port);
-app.listen(port);
+const port = 3000
+console.log('Listening at 127.0.0.1:' + port)
+app.listen(port)
--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,15 @@
 // soundcloud api key
-var SC_CLIENT_ID = '1c3aeb3f91390630d351f3c708148086';
-var STATUS_OK = 200;
+const SC_CLIENT_ID = '1c3aeb3f91390630d351f3c708148086'
+const STATUS_OK = 200
 
-SC.initialize({ client_id: SC_CLIENT_ID });
-var $player = $('#player');
+SC.initialize({client_id: SC_CLIENT_ID})
+const player = document.querySelector('#player')
 
 // TODO: soundcloud finder code
 
 /* Display SoundCloud player for the given track. */
 function displayPlayer(track) {
-  SC.oEmbed(track.permalink_url, { auto_play: true }, function(output) {
-    $player.html(output.html);
-  });
+  SC.oEmbed(track.permalink_url, {auto_play: true}, output => {
+    player.innerHTML = output.html
+  })
 }
