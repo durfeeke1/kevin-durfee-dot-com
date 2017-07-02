a1d6d4ee65ef76e3fec11e4ebd43cb02db6fb10f
Kevin Durfee <kevdurfee@gmail.com>
2017-07-02 15:23:17 -0700

    strip out soundcloud stuff

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,2 @@
-// soundcloud api key
-const SC_CLIENT_ID = '1c3aeb3f91390630d351f3c708148086'
 const STATUS_OK = 200
 
-SC.initialize({client_id: SC_CLIENT_ID})
-const player = document.querySelector('#player')
-
-// TODO: soundcloud finder code
-
-/* Display SoundCloud player for the given track. */
-function displayPlayer(track) {
-  SC.oEmbed(track.permalink_url, {auto_play: true}, output => {
-    player.innerHTML = output.html
-  })
-}
